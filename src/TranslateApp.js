40a34712bfae336edc9b9f18fd4d6ee7a4b22fc9
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-03-07 21:21:02 +0100

    feat: spell list

--- src/TranslateApp.js
+++ src/TranslateApp.js
@@ -1,132 +1,79 @@
-/* import React from "react";
-
-class TranslateApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      englishWord: "",
-      arabicWord: "",
-    };
-    this.handleEnglishWordChange = this.handleEnglishWordChange.bind(this);
-    this.handleTranslateClick = this.handleTranslateClick.bind(this);
-    this.handleSpeakEnglishClick = this.handleSpeakEnglishClick.bind(this);
-    this.handleSpeakArabicClick = this.handleSpeakArabicClick.bind(this);
-  }
-
-  handleEnglishWordChange(event) {
-    this.setState({ englishWord: event.target.value });
-  }
-
-  handleTranslateClick() {
-    const apiKey = "AIzaSyDE-wAlnykNe_rgHjTeXFpO_2h3AeIiebw";
-    const englishWord = this.state.englishWord;
-
-    fetch(
-      `https://translation.googleapis.com/language/translate/v2?key=${apiKey}&q=${englishWord}&source=en&target=ar`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const arabicWord = data.data.translations[0].translatedText;
-        this.setState({ arabicWord });
-      })
-      .catch((error) => console.error(error));
-  }
-
-  handleSpeakEnglishClick() {
-    const englishWord = this.state.englishWord;
-    const speech = new SpeechSynthesisUtterance(englishWord);
-    speech.lang = "en-US";
-    speech.rate = 1;
-    window.speechSynthesis.speak(speech);
-  }
-
-  handleSpeakArabicClick() {
-    const arabicWord = this.state.arabicWord;
-    const speech = new SpeechSynthesisUtterance(arabicWord);
-    speech.lang = "ar-SA";
-    speech.rate = 1;
-    window.speechSynthesis.speak(speech);
-  }
-
-  render() {
-    return (
-      <div>
-        <label htmlFor="englishWord">English word:</label>
-        <input
-          type="text"
-          id="englishWord"
-          value={this.state.englishWord}
-          onChange={this.handleEnglishWordChange}
-        />
-        <button onClick={this.handleTranslateClick}>Translate</button>
-        <br />
-        <label htmlFor="arabicWord">Arabic word:</label>
-        <span id="arabicWord">{this.state.arabicWord}</span>
-        <br />
-        <button onClick={this.handleSpeakEnglishClick}>Speak English</button>
-        <button onClick={this.handleSpeakArabicClick}>Speak Arabic</button>
-      </div>
-    );
-  }
-}
-
-export default TranslateApp;
- */
 import React from "react";
 
 class TranslateApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      englishWord: "",
-      arabicWord: "",
+      englishWords: "",
+      arabicWords: [],
     };
     this.handleWordChange = this.handleWordChange.bind(this);
     this.handleSpeak = this.handleSpeak.bind(this);
   }
 
   handleWordChange(event) {
-    this.setState({ englishWord: event.target.value });
+    this.setState({ englishWords: event.target.value });
   }
 
   handleSpeak(text, lang) {
-    const speech = new SpeechSynthesisUtterance(text);
-    speech.lang = lang;
-    speech.rate = 1;
-    window.speechSynthesis.speak(speech);
+    return new Promise((resolve, reject) => {
+      const speech = new SpeechSynthesisUtterance(text);
+      speech.lang = lang;
+      speech.rate = 1;
+      speech.onend = () => resolve();
+      speech.onerror = (error) => reject(error);
+      window.speechSynthesis.speak(speech);
+    });
   }
 
-  handleTranslateAndSpeak() {
+  async handleTranslateAndSpeak() {
     const apiKey = "AIzaSyDE-wAlnykNe_rgHjTeXFpO_2h3AeIiebw";
-    const englishWord = this.state.englishWord;
+    const englishWords = this.state.englishWords.split(" ");
+    const arabicWords = [];
 
-    fetch(
-      `https://translation.googleapis.com/language/translate/v2?key=${apiKey}&q=${englishWord}&source=en&target=ar`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const arabicWord = data.data.translations[0].translatedText;
-        this.handleSpeak(`${englishWord}`, "en-US");
-        setTimeout(() => this.handleSpeak(`${arabicWord}`, "ar-SA"), 2000);
-        this.setState({ arabicWord });
-      })
-      .catch((error) => console.error(error));
+    for (const englishWord of englishWords) {
+      const response = await fetch(
+        `https://translation.googleapis.com/language/translate/v2?key=${apiKey}&q=${englishWord}&source=en&target=ar`
+      );
+      const data = await response.json();
+      const arabicWord = data.data.translations[0].translatedText;
+      arabicWords.push(arabicWord);
+      await this.handleSpeak(englishWord, "en-US");
+      await this.handleSpeak(arabicWord, "ar-SA");
+    }
+
+    this.setState({ arabicWords });
   }
 
   render() {
+    const wordSpelling = this.state.englishWords
+      .split(" ")
+      .map((word, index) => {
+        const arabicWord = this.state.arabicWords[index];
+        if (arabicWord) {
+          return (
+            <React.Fragment key={index}>
+              <p>{word}</p>
+              <p>{arabicWord}</p>
+            </React.Fragment>
+          );
+        }
+        return <p key={index}>{word}</p>;
+      });
+
     return (
       <div>
-        <label htmlFor="englishWord">English word:</label>
+        <label htmlFor="englishWords">English words:</label>
         <input
           type="text"
-          id="englishWord"
-          value={this.state.englishWord}
+          id="englishWords"
+          value={this.state.englishWords}
           onChange={this.handleWordChange}
         />
         <button onClick={this.handleTranslateAndSpeak.bind(this)}>Speak</button>
         <br />
-        <label htmlFor="arabicWord">Arabic word:</label>
-        <span id="arabicWord">{this.state.arabicWord}</span>
+        <label htmlFor="arabicWords">Spelling:</label>
+        <div id="arabicWords">{wordSpelling}</div>
       </div>
     );
   }
